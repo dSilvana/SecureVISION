b28fbbe3e2a806f58684d7cfcb4132df14d7c6c0
dSilvana <57536057+dSilvana@users.noreply.github.com>
2025-12-16 22:22:33 -0500

    Add files via upload

--- AddRoundKey.vhd
+++ AddRoundKey.vhd
@@ -23,19 +23,16 @@ use IEEE.NUMERIC_STD.ALL;
 use work.aes_package.all;
 
 entity AddRoundKey is
-  Port (init_state_bytes : in block_state_type; -- Incoming bytes array
-        eic_keys : in round_key_type;
-        next_state_bytes : out block_state_type --Outgoing bytes array
+  Port (addrk_in : in block_state_type; -- Incoming ciphertext bytes array
+        round_key : in round_key_128_array; --in std_logic_vector(127 downto 0);
+        round_sel : in integer range 0 to 10;
+        addrk_out : out std_logic_vector(127 downto 0) --output as vector
       );
 end AddRoundKey;
 
 architecture Behavioral of AddRoundKey is
-
 begin
-
-    addroundkey: for i in 0 to 15 generate
-    next_state_bytes(i) <= init_state_bytes(i) xor eic_keys(i);
     
-  end generate addroundkey;
+   addrk_out <= unpack_state_array(addrk_in) xor round_key(round_sel);
   
 end Behavioral;
